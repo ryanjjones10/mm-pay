85349c8c142e4fa4c6dc47521078dc9173a4eb59
Howard Braham <howrad@gmail.com>
2023-05-05 15:48:28 -0400

    --switched to using Yarn v3 --updated all packages that could be updated --installed Prettier

--- mm-pay/app/index.js
+++ mm-pay/app/index.js
@@ -1,11 +1,11 @@
 import { View, Text } from 'react-native';
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
 
 export default Home;
